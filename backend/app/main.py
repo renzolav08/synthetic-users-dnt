c157132fddd46bb8957c89598a274f24593bc4f4
Renzo Lavado <166524036+renzolav08@users.noreply.github.com>
2026-05-23 09:16:06 -0500

    fix: agregar Vercel al CORS del backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,11 +14,16 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=[
+        "http://localhost:3000",
+        "https://synthetic-users-dnt-three.vercel.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 
